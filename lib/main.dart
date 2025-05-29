1834acf0956328ef1d24450571c591473505d332
Lobotwchs <moransamir461@gmail.com>
2025-05-29 10:02:20 -0500

    Update main.dart
    FEAT-> TITLE UPDATE

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gestor de GYM?',
       theme: ThemeData(
         // This is the theme of your application.
         //
